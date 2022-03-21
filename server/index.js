39ca0882c53c639412859478aba59678f920a481
Benjamin Ettrup Larsen <46682404+BenjaEttrup@users.noreply.github.com>
2022-03-21 10:32:06 +0100

    Made the getAllRecipes endpoint (#18)
    * Made the getAllRecipes endpoint

--- server/index.js
+++ server/index.js
@@ -9,10 +9,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/findProduct/:productName', async (req, res) => {
-    const config = {
-        headers: { 'Authorization': `Bearer ${token}`}
-    }
-    
     try{
         let apiResponse = await axios.get('https://api.sallinggroup.com/v1-beta/product-suggestions/relevant-products?query=' + req.params.productName, config).then((res) => {
             return res.data;
@@ -24,6 +20,157 @@ app.get('/findProduct/:productName', async (req, res) => {
     }
 });
 
+app.get('/findAllRecipes', async (req, res) => {
+    const recipeData = require('../opskrifter/recipes.json');
+
+    const config = {
+        headers: { 'Authorization': `Bearer ${token}`}
+    }
+
+    var recipeObjects = {
+        recipes: []
+    };
+
+    //Builds recipeObjects object from a recipe file and then searches 
+    //the salling API for the recipes ingredients.
+    for (let index1 = 0; index1 < recipeData.recipes.length; index1++) {
+        const tempRecipe = recipeData.recipes[index1];
+        
+        var recipeObject = {
+            recipe: {},
+            ingredients: []
+        };
+
+        recipeObject.recipe = tempRecipe;
+
+        for (let index2 = 0; index2 < tempRecipe.ingredients.length; index2++) {
+            const tempIngredient = recipeData.recipes[index1].ingredients[index2];
+            
+            try{
+                let apiResponse = await axios.get('https://api.sallinggroup.com/v1-beta/product-suggestions/relevant-products?query=' + tempIngredient, config).then((res) => {
+                    return res.data;
+                });
+                apiResponse.suggestions.sort(comparePrice);
+                recipeObject.ingredients.push(apiResponse.suggestions[0])
+            } catch(e) {
+                console.error(e);
+                res.status(500).send();
+            }
+        }
+
+        recipeObjects.recipes.push(recipeObject);
+    }
+
+    //Filters the recipes given some search params. This can be a string for 
+    //title or two number for max and min price
+    if(Object.keys(req.query).length !== 0) {
+        if(req.query.search !== undefined){
+            let searchedRecipes = [];
+            
+            stringRecipeSearch(req.query.search, recipeObjects.recipes).forEach((recipe) => {
+                searchedRecipes.push(recipe);
+            })
+
+            let notAlreadyChosen = recipeObjects.recipes.filter((recipe) => {
+                return !searchedRecipes.includes(recipe);
+            })
+
+            stringIngredientSearch(req.query.search, notAlreadyChosen).forEach((recipe) => {
+                searchedRecipes.push(recipe)
+            })
+
+            recipeObjects.recipes = searchedRecipes;
+        }
+
+        if(req.query.maxPrice !== undefined && req.query.minPrice !== undefined){
+            let minPrice = req.query.minPrice;
+            let maxPrice = req.query.maxPrice;
+
+            let searchedRecipes = betweenPricesSearch(minPrice, maxPrice, recipeObjects.recipes)
+
+            recipeObjects.recipes = searchedRecipes;
+        }
+    }
+
+    res.json(recipeObjects);
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
+
+/**
+ * Compare the price of two items and return the difference
+ * @param a - The first item to compare.
+ * @param b - The second value to compare.
+ * @returns The function is being called with the arguments of a and b. The function is then returning
+ * the result of a.price - b.price.
+ */
+function comparePrice(a, b) {
+    return a.price - b.price;
+}
+
+/**
+ * Given a search value and a list of recipes, return a list of recipes that contain the search value
+ * in the title
+ * @param searchValue - The string that you want to search for.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes that match the search value.
+ */
+function stringRecipeSearch(searchValue, recipes) {
+    let returnRecipes = [];
+    recipes.forEach((recipe) => {
+        let recipeTitleLowerCase = recipe.recipe.title.toLowerCase();
+        if(recipeTitleLowerCase.includes(searchValue.toLowerCase())){
+            returnRecipes.push(recipe);
+        }
+    })
+    return returnRecipes;
+}
+
+/**
+ * Given a search value and a list of recipes, return a list of recipes that include the search value
+ * in their ingredients
+ * @param searchValue - The string that you want to search for in the recipe ingredients.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes that contain the search value in their ingredients.
+ */
+function stringIngredientSearch(searchValue, recipes) {
+    let returnRecipes = [];
+    recipes.forEach((recipe) => {
+        let doesIncludeIngredient = false;
+        recipe.ingredients.forEach((ingredient) => {
+            let ingredientTitleLowerCase = ingredient.title.toLowerCase();
+            if(ingredientTitleLowerCase.includes(searchValue.toLowerCase())){
+                doesIncludeIngredient = true;
+            }
+        })
+        if(doesIncludeIngredient) {
+            returnRecipes.push(recipe);
+        }
+    })
+    return returnRecipes;
+}
+
+/**
+ * Given a list of recipes, return a list of recipes that fall between a min and max price
+ * @param minPrice - The minimum price you want to pay for your recipe.
+ * @param maxPrice - The maximum price you want to pay for your recipe.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes.
+ */
+function betweenPricesSearch(minPrice, maxPrice, recipes) {
+    let returnRecipe = [];
+    recipes.forEach((recipe) => {
+        let price = 0;
+
+        recipe.ingredients.forEach((ingredient) => {
+            price += ingredient.price;
+        })
+
+        if(price >= minPrice && price <= maxPrice){
+            returnRecipe.push(recipe);
+        }
+    })
+    return returnRecipe;
+}
