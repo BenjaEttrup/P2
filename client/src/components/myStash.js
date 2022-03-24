c735a0c9222c904b988ec1be479a2b9362a11e9d
Charlotte Elleby <celleb21@studendt.aau.dk>
2022-03-24 15:28:47 +0100

    component på stash list

--- client/src/components/myStash.js
+++ client/src/components/myStash.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../stylesheets/myStash.css'
+import StashList from './myStashList';
 
 
 //This is a React class it extends a React component which 
@@ -35,7 +36,7 @@ class MyStash extends React.Component {
             <div class="row">
               <div class="col-6">
                 <div class="input-group rounded">
-                  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon"/>
+                  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" onKeyUp="stashSearch()" />
                   <span class="input-group-text border-0" id="search-addon">
                     <i class="fa fa-search" aria-hidden="true"></i>
                   </span>
@@ -55,33 +56,13 @@ class MyStash extends React.Component {
               </thead>
               <tbody>
                 <tr>
-                  <td></td>
+                  <td>Pizza</td>
                   <td>49,95 kr.</td>
                   <td>
                     <i class="fa fa-trash"></i>
                   </td>
                 </tr>
-                <tr>
-                  <td>Pasta</td>
-                  <td>49,95 kr.</td>
-                  <td>
-                    <i class="fa fa-trash"></i>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Pasta</td>
-                  <td>49,95 kr.</td>
-                  <td>
-                    <i class="fa fa-trash"></i>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Pasta</td>
-                  <td>49,95 kr.</td>
-                  <td>
-                    <i class="fa fa-trash"></i>
-                  </td>
-                </tr>
+                <StashList/>
               </tbody>
             </table>
           </div>
