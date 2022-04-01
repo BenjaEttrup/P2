48063189003f29c50cd75d4c043941954a8b5ee5
MadsSR <madssr2@gmail.com>
2022-04-02 01:49:51 +0200

    Updated styles for myStash.js

--- client/src/components/myStash.js
+++ client/src/components/myStash.js
@@ -1,84 +1,91 @@
-import React from 'react';
-import '../stylesheets/myStash.css'
-import StashRowElement from './stashRowElement';
-import SearchStashProduct from './searchStashProduct';
+import React from "react";
+import "../stylesheets/myStash.css";
+import StashRowElement from "./stashRowElement";
+import SearchStashProduct from "./searchStashProduct";
 
-//This is a React class it extends a React component which 
+//This is a React class it extends a React component which
 //means that you can use all the code from the React component and it runs the
 //standart code in the React component
 class MyStash extends React.Component {
-  //This is a contructor this function gets called when a object gets created 
+  //This is a contructor this function gets called when a object gets created
   //from the App class. It is often used to set the values in the object
   constructor(recipe) {
-    //Super has to be called as the first thing 
+    //Super has to be called as the first thing
     //this says that the code from the React component
     //runs before our code in the contructor
     super();
 
     //Your code here
     this.state = {
-      products: []
-    }
+      products: [],
+    };
   }
-
 
   //Functions go here
   componentDidMount() {
-    fetch("/stash/get").then((response) => response.json()).then(response => {
-      this.setState({ products: response }, () => {
-        console.log(this.state.products)
+    fetch("/stash/get")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ products: response }, () => {
+          console.log(this.state.products);
+        });
       })
-    }).catch((e) => console.log(e))
+      .catch((e) => console.log(e));
   }
 
   updateTable(id) {
-    let updatedProducts = []
+    let updatedProducts = [];
     this.state.products.forEach((product) => {
       if (product.prod_id === id && product.amount > 1) {
-        product.amount --;
-        updatedProducts.push(product)
-      }
-      else if (product.prod_id !== id)
-        updatedProducts.push(product)
-    })
+        product.amount--;
+        updatedProducts.push(product);
+      } else if (product.prod_id !== id) updatedProducts.push(product);
+    });
     this.setState({
-      products: updatedProducts
-    })
+      products: updatedProducts,
+    });
   }
-
 
   //This is the render function. This is where the
   //html is.
   render() {
     return (
-      < div className="MyStash" >
+      <div className="MyStash">
         <div id="myStash" class="card shadow">
           <div class="card-body myStash-card-body">
-            <h4 class="card-title">My Stash</h4>
-            <div>
-              <SearchStashProduct updateFunction={() => this.componentDidMount()} />
+            <h2 class="card-title">My Stash</h2>
+            <div id="search-bar">
+              <SearchStashProduct
+                updateFunction={() => this.componentDidMount()}
+              />
             </div>
-            <table class="table table-striped">
+            <table
+              class="table table-striped table-borderless"
+              id="stash-table"
+            >
               <thead>
                 <tr>
-                  <th class="col-11" scope='col'>Product</th>
-                  <th scope='col'>Amount</th>
-                  <th scope='col'></th>
+                  <th class="col-11" scope="col">
+                    Product
+                  </th>
+                  <th scope="col">Amount</th>
+                  <th scope="col"></th>
                 </tr>
               </thead>
               <tbody>
-                {
-                  this.state.products.map((product) => {
-                    return (
-                      <StashRowElement product={product} updateFunction={(id) => this.updateTable(id)} />
-                    )
-                  })
-                }
+                {this.state.products.map((product) => {
+                  return (
+                    <StashRowElement
+                      product={product}
+                      updateFunction={(id) => this.updateTable(id)}
+                    />
+                  );
+                })}
               </tbody>
             </table>
           </div>
         </div>
-      </div >
+      </div>
     );
   }
 }
