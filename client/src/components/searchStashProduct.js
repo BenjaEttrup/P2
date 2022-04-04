40f6a19eae0c34f1ba56a0589a5ad1e29f978d41
Mads Risager <55142245+MadsSR@users.noreply.github.com>
2022-04-04 12:48:43 +0200

    Mystash search (#31)
    * Updated styles for myStash.js
    
    * Fixed CSS on My Stash searchbar
    
    Co-authored-by: Benjamin Ettrup Larsen <46682404+BenjaEttrup@users.noreply.github.com>

--- client/src/components/searchStashProduct.js
+++ client/src/components/searchStashProduct.js
@@ -1,13 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
-//This is a React class it extends a React component which 
+//This is a React class it extends a React component which
 //means that you can use all the code from the React component and it runs the
 //standart code in the React component
 class SearchStashProduct extends React.Component {
-  //This is a contructor this function gets called when a object gets created 
+  //This is a contructor this function gets called when a object gets created
   //from the App class. It is often used to set the values in the object
   constructor(props) {
-    //Super has to be called as the first thing 
+    //Super has to be called as the first thing
     //this says that the code from the React component
     //runs before our code in the contructor
     super();
@@ -15,72 +15,88 @@ class SearchStashProduct extends React.Component {
     this.state = {
       products: [],
       showDropdown: true,
-      searchValue: ""
-    }
-
+      searchValue: "",
+    };
   }
 
   //Functions go here
   changeSearchValue(evt) {
-    this.setState({
-      searchValue: evt.target.value
-    }, () => {
-      this.getIngredient()
-    })
+    this.setState(
+      {
+        searchValue: evt.target.value,
+      },
+      () => {
+        this.getIngredient();
+      }
+    );
   }
   getIngredient() {
     fetch(`/stash/search/${this.state.searchValue}`, {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }).then(res => res.json()).then((res) => {
-      console.log(res) //If this is false, server is down!
-      this.setState({
-        products: res.suggestions
-      }, () => { console.log(this.state.products) })
-    }).catch(err => {
-      console.error(err);
-    });
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res); //If this is false, server is down!
+        this.setState(
+          {
+            products: res.suggestions,
+          },
+          () => {
+            console.log(this.state.products);
+          }
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   addIngredient(product) {
-    console.log(product)
-    let productData = { prod_id: parseInt(product.prod_id), title: product.title, amount: 1, unit: "stk" }
-    console.log(productData)
+    console.log(product);
+    let productData = {
+      prod_id: parseInt(product.prod_id),
+      title: product.title,
+      amount: 1,
+      unit: "stk",
+    };
+    console.log(productData);
     fetch(`/stash/add`, {
-      method: 'POST',
-      mode: 'cors',
+      method: "POST",
+      mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      body: JSON.stringify(productData)
-    }).catch(err => {
-      console.error(err);
-    }).then(() => {
-      this.setState({
-        showDropdown: false
+      body: JSON.stringify(productData),
+    })
+      .catch((err) => {
+        console.error(err);
       })
-      this.props.updateFunction()
-    }
-    )
-  };
+      .then(() => {
+        this.setState({
+          showDropdown: false,
+        });
+        this.props.updateFunction();
+      });
+  }
 
-  handleBlur = evt => {
+  handleBlur = (evt) => {
     console.log("Blur evt");
     if (!evt.currentTarget.contains(evt.relatedTarget)) {
       console.log("Blur event if");
-      this.setState({ showDropdown: false })
+      this.setState({ showDropdown: false });
     }
   };
 
   resetSearch(evt) {
-    console.log("reset!")
+    console.log("reset!");
     this.setState({
-      searchValue: ""
-    })
-    this.setState({ products: [] })
+      searchValue: "",
+    });
+    this.setState({ products: [] });
   }
 
   /* this.state.product.prod_ID */
@@ -89,32 +105,54 @@ class SearchStashProduct extends React.Component {
   //html is.
   render() {
     return (
-      <div className="SearchStashProduct" >
-        <div className="searchWidth" onBlur={this.handleBlur} onFocus={() => { this.setState({ showDropdown: true }) }
-        } >
-          <div class="input-group rounded">
-            <input type="search" class="form-control rounded" value={this.state.searchValue} placeholder="Search" aria-label="Search" aria-describedby="search-addon"
-              onChange={(evt) => { this.changeSearchValue(evt); this.setState({ showDropdown: true }) }} />
-            <span class="input-group-text border-0" id="search-addon">
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </span>
+      <div className="SearchStashProduct">
+        <div
+          className="searchWidth"
+          onBlur={this.handleBlur}
+          onFocus={() => {
+            this.setState({ showDropdown: true });
+          }}
+        >
+          <div class="form rounded">
+            <i class="fa fa-search"></i>
+            <input
+              type="search"
+              class="form-control rounded search-bar"
+              value={this.state.searchValue}
+              placeholder="Add item to my stash..."
+              aria-label="Search"
+              aria-describedby="search-addon"
+              onChange={(evt) => {
+                this.changeSearchValue(evt);
+                this.setState({ showDropdown: true });
+              }}
+            />
           </div>
-          <div class="searchResults" >
-            <ul className="list-group" >
-              {this.state.showDropdown ? (this.state.products !== undefined ? this.state.products.map((product) => {
-                return (
-                  <button type="button" className="list-group-item list-group-item-action" onClick={() => { this.addIngredient(product); this.resetSearch() }}>
-                    {product.title}
-                  </button>
-                )
-              }) : "") : ""
-              }
+          <div class="searchResults">
+            <ul className="list-group">
+              {this.state.showDropdown
+                ? this.state.products !== undefined
+                  ? this.state.products.map((product) => {
+                      return (
+                        <button
+                          type="button"
+                          className="list-group-item list-group-item-action"
+                          onClick={() => {
+                            this.addIngredient(product);
+                            this.resetSearch();
+                          }}
+                        >
+                          <i class="fa fa-plus-circle searchResult"></i>
+                          {product.title}
+                        </button>
+                      );
+                    })
+                  : ""
+                : ""}
             </ul>
           </div>
         </div>
-      </div >
-
-
+      </div>
     );
   }
 }
--- client/src/components/stashRowElement.js
+++ client/src/components/stashRowElement.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
-
-//This is a React class it extends a React component which 
+//This is a React class it extends a React component which
 //means that you can use all the code from the React component and it runs the
 //standart code in the React component
 class StashRowElement extends React.Component {
-  //This is a contructor this function gets called when a object gets created 
+  //This is a contructor this function gets called when a object gets created
   //from the App class. It is often used to set the values in the object
   constructor(props) {
-    //Super has to be called as the first thing 
+    //Super has to be called as the first thing
     //this says that the code from the React component
     //runs before our code in the contructor
     super();
@@ -19,12 +18,12 @@ class StashRowElement extends React.Component {
 
   removeIngredient() {
     fetch(`/stash/remove/${this.props.product.prod_id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }).catch(err => {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }).catch((err) => {
       console.error(err);
     });
     this.props.updateFunction(this.props.product.prod_id);
@@ -35,12 +34,23 @@ class StashRowElement extends React.Component {
   //html is.
   render() {
     return (
-      <tr>
+      <tr class="table-content  table-rounded">
         <td>{this.props.product ? this.props.product.title : ""}</td>
-        <td>{this.props.product ? this.props.product.amount : ""} {this.props.product ? this.props.product.unit : ""}</td>
-        <td>
-          <button type="button" class="deleteButton" onClick={() => { this.removeIngredient() }}>
-            <i class="fa fa-trash"></i></button>
+        <td class="table-content-secondary">
+          {this.props.product
+            ? this.props.product.amount + " " + this.props.product.unit
+            : ""}
+        </td>
+        <td class="right-align-text">
+          <button
+            type="button"
+            class="deleteButton"
+            onClick={() => {
+              this.removeIngredient();
+            }}
+          >
+            <i class="fa fa-trash"></i>
+          </button>
         </td>
       </tr>
     );
