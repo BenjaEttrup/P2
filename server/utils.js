45f4c10fc843069a259235e9d9d87ea8b93a24a5
Charlotte Elleby <charlotte@h324.aau1x.klient.str.site.aau.dk>
2022-05-10 09:10:55 +0200

    back-end tests

--- server/utils.js
+++ server/utils.js
@@ -63,18 +63,20 @@ module.exports = {
     } while (currentDate - date < milliseconds);
   },
   /**
-   * This function finds the index of a recipe in the file.
-   * @param ID - The ID of the recipe you're looking for.
-   * @param filePath - The path to the file you want to search.
-   * @param option - member we want to acess in the file.
-   * @returns The index of the recipe.
+   * This function takes in a recipe ID, a data object, and an option string. It then returns the index
+   * of the recipe in the data object
+   * @param ID - The ID of the recipe you want to find
+   * @param data - the data object that contains the recipes and shopping list
+   * @param option - "shoppingList" or "recipes"
+   * @returns The index of the recipe in the shopping list or the recipe list.
    */
   findRecipeIndex(ID, data, option) {
     let numberID = Number.parseInt(ID);
     let returnValue = false;
     if (option === "shoppingList") {
       let index = 0;
-      data[option].forEach((recipe) => {
+
+      data.shoppingList.forEach((recipe) => {
         if (recipe.recipe.recipeID === numberID) {
           returnValue = index;
         }
@@ -87,8 +89,101 @@ module.exports = {
         }
       }
     }
-
     return returnValue;
     // TODO reevaluate this function design
+  },
+  /**
+ * Find the index of an ingredient in a recipe
+ * @param recipeIndex - The index of the recipe in the user's recipe list.
+ * @param productID - The ID of the product you want to find.
+ * @param filePath - the path to the user data file
+ * @param option - the member we want to access in the user.json file
+ * @returns The index of the ingredient in the recipe.
+ */
+findIngredientIndex(recipeIndex, productID, filePath, option) {
+  let userData = require(filePath);
+  productID = Number(productID);
+
+  // Loops through the ingredients found in userData.shoppinglist[recipeIndex].ingredients and compares prod ID.
+  for (ingredient in userData[option][recipeIndex].ingredients) {
+    if (
+      Number(userData[option][recipeIndex].ingredients[ingredient].prod_id) ===
+      productID
+    ) {
+      return ingredient;
+    }
   }
+},
+/**
+ * Compare the price of two items and return the difference
+ * @param a - The first item to compare.
+ * @param b - The second value to compare.
+ * @returns The function is being called with the arguments of a and b. The function is then returning
+ * the result of a.price - b.price.
+ */
+ comparePrice(a, b) {
+  return a.price - b.price;
+},
+/**
+ * Given a search value and a list of recipes, return a list of recipes that contain the search value
+ * in the title
+ * @param searchValue - The string that you want to search for.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes that match the search value.
+ */
+ stringRecipeSearch(searchValue, recipes) {
+  let returnRecipes = [];
+  recipes.forEach((recipe) => {
+    let recipeTitleLowerCase = recipe.recipe.title.toLowerCase();
+    if (recipeTitleLowerCase.includes(searchValue.toLowerCase())) {
+      returnRecipes.push(recipe);
+    }
+  });
+  return returnRecipes;
+},
+/**
+ * Given a search value and a list of recipes, return a list of recipes that include the search value
+ * in their ingredients
+ * @param searchValue - The string that you want to search for in the recipe ingredients.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes that contain the search value in their ingredients.
+ */
+ stringIngredientSearch(searchValue, recipes) {
+  let returnRecipes = [];
+  recipes.forEach((recipe) => {
+    let doesIncludeIngredient = false;
+    recipe.ingredients.forEach((ingredient) => {
+      let ingredientTitleLowerCase = ingredient.title.toLowerCase();
+      if (ingredientTitleLowerCase.includes(searchValue.toLowerCase())) {
+        doesIncludeIngredient = true;
+      }
+    });
+    if (doesIncludeIngredient) {
+      returnRecipes.push(recipe);
+    }
+  });
+  return returnRecipes;
+},
+/**
+ * Given a list of recipes, return a list of recipes that fall between a min and max price
+ * @param minPrice - The minimum price you want to pay for your recipe.
+ * @param maxPrice - The maximum price you want to pay for your recipe.
+ * @param recipes - an array of recipes
+ * @returns An array of recipes.
+ */
+ betweenPricesSearch(minPrice, maxPrice, recipes) {
+  let returnRecipe = [];
+  recipes.forEach((recipe) => {
+    let price = 0;
+
+    recipe.ingredients.forEach((ingredient) => {
+      price += ingredient.price;
+    });
+
+    if (price >= minPrice && price <= maxPrice) {
+      returnRecipe.push(recipe);
+    }
+  });
+  return returnRecipe;
+}
 }
--- server/utils.test.js
+++ server/utils.test.js
@@ -13,5 +13,84 @@ test('Testing if callApi always gives an answer', () => {
 })
 
 test('Testing findRecipeIndex', () => {
+    let recipeObject = require("./recipe.test.json")
+    let userObject = require("./user.test.json")
+    expect(utils.findRecipeIndex('1', recipeObject, "recipes")).toBe("1");
+    expect(utils.findRecipeIndex('ø', recipeObject, "recipes")).toBe(false);
+    expect(utils.findRecipeIndex('i', userObject, "recipes")).toBe(false);
+    expect(utils.findRecipeIndex('1', userObject, "shoppingList")).toBe(1);
+    expect(utils.findRecipeIndex('a', userObject, "shoppingList")).toBe(false);
+    expect(utils.findRecipeIndex('1', recipeObject, "list")).toBe(false);
+})
 
+test('Testing findIngredientIndex', ()=>{
+    let userObject = "./user.test.json"
+    expect(utils.findIngredientIndex("1", "63861", userObject, "shoppingList")).toBe("1")
+    expect(utils.findIngredientIndex("2", "63861", userObject, "shoppingList")).toBe(undefined)
+    expect(utils.findIngredientIndex("3", "51061", userObject, "shoppingList")).toBe("11")
+    expect(utils.findIngredientIndex("3", "63861", userObject, "shoppingList")).toBe(undefined)
+    expect(utils.findIngredientIndex("0", "20555", userObject, "shoppingList")).toBe("0")
+})
+
+test('Testing comparePrice', () => {
+    let suggestionsObject = require("./suggestions.test.json")
+    expect(utils.comparePrice(suggestionsObject.suggestions[1],suggestionsObject.suggestions[2])).toBe(0.25)
+    expect(utils.comparePrice(suggestionsObject.suggestions[1],suggestionsObject.suggestions[1])).toBe(0)
+})
+
+test ('Testing stringRecipeSearch', () => {
+    let recipeObject = require("./recipeObject.test.json")
+    expect(utils.stringRecipeSearch("Svampe risotto", recipeObject)).toStrictEqual([])
+    expect(utils.stringRecipeSearch("Simpel wokret", recipeObject)).toStrictEqual(
+        [{"recipe": {
+          "title": "Simpel wokret"},
+          "ingredients" : [
+                            {"kyllingebryst": 
+                                            {
+                                              "amount" : 1,
+                                              "unit": "stk"
+                                              },
+                              "title" :"Kyllingebryst",
+                              "price" : 10.00
+                                            },
+                           {"wokblanding": {"amount": 0.5,"unit": "stk"}, "title" :"Wokblanding", "price" : 10.00}]}])
+    expect(utils.stringRecipeSearch("1", recipeObject)).toStrictEqual([])
+})
+
+test ('Testing stringIngredientSearch', () => {
+    let recipeObject = require("./recipeObject.test.json")
+    expect(utils.stringIngredientSearch("wokblanding",recipeObject)).toStrictEqual(
+        [{"recipe": {
+          "title": "Simpel wokret"},
+          "ingredients" : [
+                            {"kyllingebryst": 
+                                            {
+                                              "amount" : 1,
+                                              "unit": "stk"
+                                              },
+                              "title" :"Kyllingebryst",
+                              "price" : 10.00
+                                            },
+                           {"wokblanding": {"amount": 0.5,"unit": "stk"}, "title" :"Wokblanding", "price" : 10.00}]}])
+    expect(utils.stringIngredientSearch("pasta",recipeObject)).toStrictEqual([])
+    expect(utils.stringIngredientSearch("8",recipeObject)).toStrictEqual([])
+    
+})
+
+test ('Testing betweenPricesSearch', () => {
+    let recipeObject = require("./recipeObject.test.json")
+    expect(utils.betweenPricesSearch(39,41,recipeObject)).toStrictEqual([])
+    expect(utils.betweenPricesSearch(19,21,recipeObject)).toStrictEqual(
+    [{"recipe": {
+    "title": "Simpel wokret"},
+    "ingredients" : [
+                      {"kyllingebryst": 
+                                      {
+                                        "amount" : 1,
+                                        "unit": "stk"
+                                        },
+                        "title" :"Kyllingebryst",
+                        "price" : 10.00
+                                      },
+                     {"wokblanding": {"amount": 0.5,"unit": "stk"}, "title" :"Wokblanding", "price" : 10.00}]}])
 })
